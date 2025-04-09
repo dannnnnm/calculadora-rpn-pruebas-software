2ff9c3a0fe4989d3d89585ce213e05eed68e99ac
Daniel <d.aguayo02@ufromail.cl>
2025-04-09 17:39:29 -0400

    agregar tests adicionales

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Random;
 
+//Alumnos: Vicente Marquez
+//         Daniel Aguayo
+
+
 public class CalculadoraTest {
 
     RPNCalculator rpnCalculator;
@@ -23,6 +27,48 @@ public class CalculadoraTest {
         rpnCalculator = null;
         stack=null;
     }
+
+
+    //Test agregados para revisar tamaño de números
+
+    @Test
+    void allowOperationsOnAlmostBigNumbers(){
+        //Permite operaciones con numeros cercanos a los grandes siempre que el resultado sea menor al límite de 10 cifras
+        String expression = "99999999 43 +";
+        Assertions.assertDoesNotThrow(()->rpnCalculator.calculate(expression));
+    }
+
+    @Test
+    void throwErrorOnBigResult(){
+        //Si el resultado es más grande de 10 cifras, lanza error
+        String expression= "99999999999 2 *";
+        InvalidOperatorException exception = Assertions.assertThrows(InvalidOperatorException.class,
+                ()->rpnCalculator.calculate(expression),
+                "number too big");
+
+        Assertions.assertTrue(exception.getMessage().contains("too big"));
+    }
+
+    @Test
+    void considersDecimalNumberUpToLimit(){
+        //Considera números decimales hasta un límite de seis dígitos decimales
+        String expression="0.000001 2.5 +";
+        CalcResult result=rpnCalculator.calculate(expression);
+
+        Assertions.assertEquals(2.500001, result.inner());
+    }
+
+    @Test
+    void discardsDigitsAfterLimit(){
+        //descarta dígitos decimales después del sexto.
+        String expression="0.2100001 2.5 +";
+        CalcResult result=rpnCalculator.calculate(expression);
+
+        Assertions.assertEquals(2.71, result.inner());
+    }
+
+    //Fin de tests nuevos
+
 
     @Test
     void simpleAdd(){
@@ -98,6 +144,7 @@ public class CalculadoraTest {
 
     @Test
     void invalidOperator(){
+        // Profesor: considerar para "caracter extraño" Y "operación incompleta"
         String badExpression = "8 3 $";
         InvalidOperatorException exception = Assertions.assertThrows(InvalidOperatorException.class,
                                                 ()->rpnCalculator.calculate(badExpression),
@@ -106,16 +153,21 @@ public class CalculadoraTest {
         Assertions.assertTrue(exception.getMessage().contains("operator"));
     }
 
+
+
     @Test
     void checkTiming(){
         String expression = "12 3.5 + 7 2 - * 10 2.5 / 4 + 6 1.5 - * 8 4 / + 5.5 -";
         long startTime = System.currentTimeMillis();
 
-        Result result = rpnCalculator.calculate(expression);
+        CalcResult result = rpnCalculator.calculate(expression);
         long stopTime = System.currentTimeMillis();
         long secondsElapsed = (stopTime-startTime)/1000;
         Assertions.assertTrue(secondsElapsed<5);
     }
+
+
+
 
     @Test
     void pushToStackIncreasesLength(){
@@ -156,7 +208,7 @@ public class CalculadoraTest {
             stack.push(i);
         }
         for (int i = 0; i < amountOfPops; i++) {
-            stack.pop()
+            stack.pop();
         }
         Assertions.assertEquals(stack.length(), amountOfPushes-amountOfPops);
     }
